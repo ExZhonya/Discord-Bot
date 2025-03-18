fd96439c74c8fecb4c7bab19be36f3d516e8e24b
ExZhonya <exzhonya@gmail.com>
2025-03-18 20:20:39 +0700

    Update events.py

--- Bot/cogs/events.py
+++ Bot/cogs/events.py
@@ -63,7 +63,7 @@ class Events(commands.Cog):
                     f"**Thanks for adding me to your server! 🥰**\n\n"
                     "To play game, simply type `/game` or `.game` followed with `private` or `public` session!\n"
                     "Want to see all of my command list? Then just do `.help` to see all of my available commands!\n"
-                    "If you have any questions or need assistance, feel free to join our [support server](https://your-support-server-link.com)\n\n"
+                    "If you have any questions or need assistance, feel free to join our [support server](https://discord.gg/Tfug7jMMRv)\n\n"
                     "**Thanks For Choosing Yuuki!**"
                 ),
                 color=discord.Color.pink()
